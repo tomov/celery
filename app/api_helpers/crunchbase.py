9c283ded71120e9c8b119b5f558fa71a0ba50de8
Ubuntu <ubuntu@ip-172-31-41-118.ec2.internal>
2014-11-06 01:53:52 +0000

    still adding details in crucnhbase API scraper; must add a few more and test, then connect w/ rest of stuff

--- app/api_helpers/crunchbase.py
+++ app/api_helpers/crunchbase.py
@@ -51,6 +51,17 @@ class CrunchBaseAPI:
             # fetch the page of the first result and get all the details
             result_raw = crunchbase.get(result['path'])
             result = json.loads(result_raw)
+            if 'properties' in result['data']:
+                # description
+                if 'description' in result['data']['properties']:
+                    company_info['description'] = result['data']['properties']['description']
+                # summary
+                if 'short_description' in result['data']['properties']:
+                    company_info['summary'] = result['data']['properties']['short_description']
+                # team size
+                if 'num_employees_min' in result['data']['properties'] and 'num_employees_max' in result['data']['properties']:
+                    company_info['employees_min'] = result['data']['properties']['num_employees_min']
+                    company_info['employees_max'] = result['data']['properties']['num_employees_max']
             if 'relationships' in result['data']:
                 # logo
                 if 'primary_image' in result['data']['relationships'] and len(result['data']['relationships']['primary_image']['items']) > 0:
@@ -62,40 +73,93 @@ class CrunchBaseAPI:
                         'region': result['data']['relationships']['headquarters']['items'][0].get('region')
                         'country': result['data']['relationships']['headquarters']['items'][0].get('country_code')
                     })
-            if 'properties' in result['data']:
-                # description
-                if 'description' in result['data']['properties']:
-                    company_info['description'] = result['data']['properties']['description']
-                # summary
-                if 'short_description' in result['data']['properties']:
-                    company_info['summary'] = result['data']['properties']['short_description']
+                # industries
+                industries = []
+                if 'categories' in result['data']['relationships']:
+                    for category_data in result['data']['relationships']['categories']['items']:
+                        industries.append(category_data['name'])
+                company_info['industries_json'] = json.dumps(industries)
+                # offices
+                offices = []
+                if 'offices' in result['data']['relationships']:
+                    for office_data in result['data']['relationships']['offices']['items']:
+                        offices.append({
+                            'city': office_data['city'],
+                            'region': office_data['region'],
+                            'country': office_data['country_code']
+                        })
+                company_info['offices_json'] = json.dumps(offices)
+                # funding rounds
+                funding_rounds = []
+                if 'funding_rounds' in result['data']['relationships']:
+                    for funding_round_info in result['data']['relationships']['funding_rounds']['items']:
+                        funding_round_data = json.loads(crunchbase.get(funding_round_info['path']))
+                        if 'data' in funding_round_data and 'properties' in funding_round_data['data']:
+                            funding_round = {
+                                'series':  funding_round_data['data']['properties'].get('series'),
+                                'money_raised':  funding_round_data['data']['properties'].get('money_raised'),
+                                'money_raised_currency_code': funding_round_data['data']['properties'].get('money_raised_currency_code'),
+                                'money_raised_usd':  funding_round_data['data']['properties'].get('money_raised_usd'),
+                                'post_money_valuation': funding_round_data['data']['properties'].get('post_money_valuation'),
+                                'post_money_valuation_currency_code': funding_round_data['data']['properties'].get('post_money_valuation_currency_code'),
+                                'announced_on_year': funding_round_data['data']['properties'].get('announced_on_year'),
+                                'announced_on': funding_round_data['data']['properties'].get('announced_on')
+                            })
+                        else:
+                            funding_round = dict()
+                        investments = []
+                        if 'data' in funding_round_data and 'relationships' in funding_round_data['data'] and 'investments' in funding_round_data['data']['relationships']:
+                            for investment_data in funding_round_data['data']['relationships']['investments']['items']:
+                                investment = {
+                                    'money_invested': investment_data.get('money_invested'),
+                                    'money_invested_currency_code': investment_data.get('money_invested_currency_code'),
+                                    'money_invested_usd': investment_data.get('money_invested_usd')
+                                }
+                                if 'investor' in investment_data:
+                                    investment['investor_type'] = investment_data['investor'].get('type')
+                                    if 'first_name' in investment_data['investor'] and 'last_name' in investment_data['investor']:
+                                        investment['investor'] = str(investment_data['investor']['first_name']) + ' ' + investment_data['investor']['last_name']
+                                    else:
+                                        investment['investor'] = investment_data['investor'].get('name')
+                                investments.append(investment)
+                        funding_round['investments'] = investments 
+                        funding_rounds.append(funding_round)
+                    company_info['funding_rounds_json'] = json.dumps(funding_rounds)
+                # team
+                if 'current_team' in result['data']['relationships']:
+                    team = []
+                    for team_member_info in result['data']['relationships']['current_team']['items']:
+                        team_member = {
+                            'first_name': team_member_info.get('first_name'),
+                            'last_name': team_member_info.get('last_name'),
+                            'title': team_member_info.get('title')
+                        }
+                        team_member_data = json.loads(crunchbase.get(team_member_info['path']))
+                        if 'data' in team_member_data and 'properties' in team_member_data['data']:
+                            team_member['bio'] = team_member_data.get('bio')
+                        if 'data' in team_member_data and 'relationships' in team_member_data['data']:
+                            if 'primary_image' in team_member_data['data']['relationships'] and len(team_member_data['data']['relationships']['primary_image']['items']) > 0:
+                                team_member['photo_url'] = image_prefix + team_member_data['data']['relationships']['primary_image']['items'][0]['path']
+                            if 'experience' in team_member_data['data']['relationships']:
+                                experience = []
+                                for position_data in team_member_data['data']['relationships']['experience']['items']:
+                                    position = {
+                                        'company': position_data.get('organization_name'),
+                                        'title': position_data.get('title'),
+                                        'date_started': position_data.get('started_on'),
+                                        'date_ended': position_data.get('ended_on')
+                                    }
+                                    experience.append(position)
+                                team_member['experience'] = experience
+                        team.append(team_member)
+                    company_info['team_json'] = json.dumps(team)
+
+          # TODO go through all of them and make sure
+                  # 1) all of the stuff you insert / append is added to company_info
+                  # 2) you use .get() and will not fail if stuff is missing
+
             # raw crucnhbase data 
             company_info['crunchbase_data'] = result_raw
-            # industries
-            industries = []
-            if 'categories' in result['data']['relationships']:
-                for category_data in result['data']['relationships']['categories']['items']:
-                    industries.append(category_data['name'])
-            company_info['industries_json'] = json.dumps(industries)
-            # offices
-            offices = []
-            if 'offices' in result['data']['relationships']:
-                for office_data in result['data']['relationships']['offices']['items']:
-                    offices.append({
-                        'city': office_data['city'],
-                        'region': office_data['region'],
-                        'country': office_data['country_code']
-                    })
-            company_info['offices_json'] = json.dumps(offices)
-            # funding rounds
-            funding_rounds = []
-            if 'funding_rounds' in result['data']['relationships']:
-                for funding_round_info in result['data']['relationships']['funding_rounds']['items']:
-                    funding_round_data = json.loads(crunchbase.get(funding_round_info['path']))
-                    funding_rounds.append({
-                        ''
-                    })
-            company_info['funding_rounds_json'] = json.dumps(funding_rounds)
         except:
             company_info = None
         return company_info
